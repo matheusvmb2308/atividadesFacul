f4f36b219ad364682e1e655f85b80ed00ff18a24
Matheus <matheusvmbernardes@gmail.com>
2026-03-28 21:24:30 -0300

    Ajustando a logica de aumentar o salario

--- atividadesPOO/heranca/exercicio7.py
+++ atividadesPOO/heranca/exercicio7.py
@@ -9,24 +9,28 @@ class Empregado():
         self.__salario = salario
     def getSalario(self):
         return self.__salario
-    def aumentoSalario(self, percentual):
+    def setSalario(self, novo_salario):
+        self.__salario = novo_salario
+    def setAumentaSalario(self, percentual):
         self.__salario += self.__salario * percentual / 100
         return self.__salario
 class Chefe(Empregado):
     def __init__(self, codigo, nome, email, salario, beneficio):
         super().__init__(codigo, nome, email, salario)
         self.__beneficio = beneficio
-    def aumentoSalario(self, percentual):
-        return super().aumentoSalario(percentual) + self.__beneficio
+    def setAumentaSalario(self, percentual):
+        salario = super().setAumentaSalario(percentual)
+        return self.setSalario(salario + self.__beneficio)
 class Estagiario(Empregado):
     def __init__(self, codigo, nome, email, salario, desconto):
         super().__init__(codigo, nome, email, salario)
         self.__desconto = desconto
-    def aumentoSalario(self, percentual):
-        return super().aumentoSalario(percentual) - self.__desconto
+    def setAumentaSalario(self, percentual):
+        salario = super().setAumentaSalario(percentual)
+        return self.setSalario(salario - self.__desconto)
 chefe = Chefe(1, "Carlos", "carlos@gmail.com", 10000, 500)
 estagiario = Estagiario(1, "Carlos", "carlos@gmail.com", 1000, 200)
-chefe.aumentoSalario(10)
-estagiario.aumentoSalario(10)
+chefe.setAumentaSalario(10)
+estagiario.setAumentaSalario(10)
 print(chefe.getSalario())
 print(estagiario.getSalario())
