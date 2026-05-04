9759a46d949c30b77accca6b9d3c4f5434cfda06
matheusvmb2308 <matheusvmbernardes@gmail.com>
2026-05-04 09:25:30 -0300

    Iniciando a primeira versao do grande software

---
+++ atividadesPOO/grandeSoftware/versao1.py
@@ -0,0 +1,127 @@
+from enum import Enum
+
+# Podemos nos referir a eles como Wood.SITKA ou Builder.GIBSON 
+# e evitar todas essas comparações de strings completamente
+class Builder(Enum):
+    FENDER = "fender"
+    MARTIN = "martin"
+    GIBSON = "gibson"
+    COLLINGS = "collings"
+    OLSON = "olson"
+    RYAN = "ryan"
+    PRS = "prs"
+    ANY = "any"
+
+#Cada "enum" toma o lugar de uma das propriedades de uma guitarra
+class TypeG(Enum):
+    ACOUSTIC = "acoustic"
+    ELETRIC = "eletric"
+
+
+# Podemos nos referir a eles como Wood.SITKA ou Builder.GIBSON 
+# e evitar todas essas comparações de strings completamente
+class Wood(Enum):
+    INDIAN_ROSEWOOD = "indian_rosewood"
+    BRAZILIAN_ROSEWOOD = "brazilian_rosewood"
+    MAHOGANY = "mahogany"
+    MAPLE = "maple"
+    COCOBOLO = "cocobolo"
+    CEDAR = "cedar"
+    ADIRONDACK = "adirondack"
+    ALDER = "alder"
+    SITKA = "sitka"
+
+class Guitar:
+    def __init__(self, serial_number, price, builder, model, typeg, back_wood, top_wood):
+        self.serial_number = serial_number
+        self.price = price
+        self.builder = builder
+        self.model = model
+        self.typeg = typeg
+        self.back_wood = back_wood
+        self.top_wood = top_wood
+
+    def get_serial_number(self):
+        return self.serial_number
+
+    def get_price(self):
+        return self.price
+
+    def set_price(self, new_price):
+        self.price = new_price
+
+    def get_builder(self):
+        return self.builder
+
+    def get_typeg(self):
+        return self.typeg
+
+    def get_model(self):
+        return self.model
+
+    def get_back_wood(self):
+        return self.back_wood
+
+    def get_top_wood(self):
+        return self.top_wood
+
+#Classe Inventory
+class Inventory:
+    def __init__(self):
+        self.guitars = []
+
+    def add_guitar(self, serial_number, price, builder, model, typeg, back_wood, top_wood):
+        guitar = Guitar(serial_number, price, builder, model, typeg, back_wood, top_wood)
+        self.guitars.append(guitar)
+
+    def get_guitar(self, serial_number):
+        for guitar in self.guitars:
+            if guitar.get_serial_number() == serial_number:
+                return guitar
+        return None
+
+    def search_guitar(self, search_guitar):
+        found_guitars = []
+        for guitar in self.guitars:
+            # Parece que nada mudou, mas com "Enums", não precisamos nos preocupar com essas comparações 
+            # sendo prejudicadas por erros ortográficos ou problemas de maiúscula/minúscula
+            if search_guitar.get_builder() != guitar.get_builder():
+                continue
+            
+            # A única propriedade com a qual precisamos nos preocupar é o "model", já que ainda é uma String
+            model = search_guitar.get_model().lower()
+            if model and model != "" and model != guitar.get_model().lower():
+                continue
+            
+            # Parece que nada mudou, mas com "Enums", não precisamos nos preocupar com essas comparações 
+            # sendo prejudicadas por erros ortográficos ou problemas de maiúscula/minúscula
+            if search_guitar.get_typeg() != guitar.get_typeg():
+                continue
+            if search_guitar.get_back_wood() != guitar.get_back_wood():
+                continue
+            if search_guitar.get_top_wood() != guitar.get_top_wood():
+                continue
+            found_guitars.append(guitar)
+        if found_guitars:
+            return found_guitars
+        return None
+    
+# Testando o Sistema
+
+# Set up Rick’s guitar inventory
+inventory = Inventory()
+
+# Adiciona guitarras ao estoque
+inventory.add_guitar("V95693", 1499.95, Builder.FENDER.value, "Stratocastor", TypeG.ELETRIC.value, Wood.ALDER.value, Wood.ALDER.value)
+inventory.add_guitar("V92341", 1600.00, Builder.FENDER.value, "Stratocastor", TypeG.ELETRIC.value, Wood.ALDER.value, Wood.ALDER.value)
+inventory.add_guitar("11277", 3999.95, Builder.COLLINGS.value, "Stratocastor", TypeG.ACOUSTIC.value, Wood.INDIAN_ROSEWOOD.value, Wood.INDIAN_ROSEWOOD.value)
+
+
+# Buscando por uma guitarra que o Erin gosta: Fender Stratocastor elétrica com corpo de Alder e tampo de Alder
+whatErinLikes = Guitar(" ", 0, Builder.FENDER.value, "Stratocastor", TypeG.ELETRIC.value, Wood.ALDER.value, Wood.ALDER.value)
+guitars = inventory.search_guitar(whatErinLikes)
+if guitars is not None:
+    for guitar in guitars:
+        print(f"Erin, talvez você goste desta: {guitar.get_builder()} {guitar.get_model()} {guitar.get_typeg()} guitar:\n{guitar.get_back_wood()} na traseira e laterais, {guitar.get_top_wood()} no tampo.\nEla pode ser sua por apenas US${guitar.get_price()}! \n")
+else:
+  print("Desculpe Erin, não temos nada para você")
